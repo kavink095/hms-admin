5060420665a5df289f07a1af6d0ea96a1a3b8431
Hasantha U.K. [I.T.] <hasantha.uk@arpiconet.com>
2020-07-16 17:15:38 +0530

    get all commit

--- src/app/services/addadminservice.ts
+++ src/app/services/addadminservice.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { AdminDTO } from '../dtos/adminDTO';
 
 export const MAIN_URL = 'http://localhost:8084';
+const urls = '/api/v1/admins';
 const URL = '/api/v1/admins/saves';
 
 @Injectable()
@@ -16,6 +17,6 @@ export class Addadminservice {
         return this.http.post<boolean>(MAIN_URL + URL, admin);
     }
     getAllAdmins(): Observable<Array<AdminDTO>>{
-        return this.http.get<Array<AdminDTO>>(MAIN_URL + URL);
+        return this.http.get<Array<AdminDTO>>(MAIN_URL + urls);
       }
 }
