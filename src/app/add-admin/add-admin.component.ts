3cd3c34a2c35085da77349818ad9b0b3cd90592c
Hasantha U.K. [I.T.] <hasantha.uk@arpiconet.com>
2020-09-10 14:52:22 +0530

    change admin view

--- src/app/add-admin/add-admin.component.ts
+++ src/app/add-admin/add-admin.component.ts
@@ -1,14 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-<<<<<<< HEAD
 import { Addadminservice } from '../services/addadminservice';
 import { AdminDTO } from '../dtos/adminDTO';
 import { Observable } from 'rxjs';
-=======
-import { Observable } from 'rxjs';
-import { AdminDTO } from '../dtos/adminDTO';
-import { Addadminservice } from '../services/addadminservice';
->>>>>>> ee2d70436b27e0aec2e7bc840534519dd709a10a
+
 
 @Component({
   selector: 'app-add-admin',
@@ -16,20 +11,13 @@ import { Addadminservice } from '../services/addadminservice';
   styleUrls: ['./add-admin.component.css']
 })
 export class AddAdminComponent implements OnInit {
-  // formAddAdmin: FormGroup;
+  formAddAdmin: FormGroup;
 
-<<<<<<< HEAD
+
   admins: Array<AdminDTO> = [];
   adminss: Observable<AdminDTO[]>;
 
   constructor( private adminservices: Addadminservice) { }
-=======
-
-  admins: Array<AdminDTO> = [];
-  adminss: Observable<AdminDTO[]>;
-
-  constructor(private adminservice: Addadminservice) { }
->>>>>>> ee2d70436b27e0aec2e7bc840534519dd709a10a
 
   ngOnInit(): void {
     this.reloadData();
@@ -37,11 +25,7 @@ export class AddAdminComponent implements OnInit {
 
   // tslint:disable-next-line: typedef
   reloadData() {
-<<<<<<< HEAD
     this.adminss = this.adminservices.getEmployeesList();
-=======
-    this.adminss = this.adminservice.getEmployeesList();
->>>>>>> ee2d70436b27e0aec2e7bc840534519dd709a10a
   }
 
 }
